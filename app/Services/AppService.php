4c62799bca3b0b1e1e8963ce9443fa198ba495fb
joao paulo <joaosilva@mangue3.com>
2018-11-06 11:37:37 -0300

    service end traits

---
+++ app/Services/AppService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joaopaulo
+ * Date: 06/11/18
+ * Time: 11:37
+ */
+
+namespace App\Services;
+/**
+ * Class AppService
+ * @package App\Services
+ */
+class AppService
+{
+}
